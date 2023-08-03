3c521e5adb42b2a93a2a99752d468e13b535a282
rouzbeh <rouzbehmsv1988@gmail.com>
2023-08-03 15:59:02 +0200

    combination of different animations

--- ShapesAndAnimations/ContentView.swift
+++ ShapesAndAnimations/ContentView.swift
@@ -8,14 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State var firstLevelClicked = false
+    @State var SecondLevelClicked = false
+    @State var thirdLevelClicked = false
+    @State var forthLevelClicked = false
+    @State var bounce = false
+    
+    func playButton(third: Bool) -> some View {
+        ZStack {
+            Circle().stroke(Color.red).frame(width: 200, height: 200) .background(Circle().foregroundColor(third ? Color.red: .clear))
+            PolygonShape(sides:3).fill(Color.red).frame(width: 100, height: 100)
         }
-        .padding()
+    }
+    var body: some View {
+        ZStack{
+            if thirdLevelClicked {
+                Image("MatrixPills").background(.white).scaleEffect(forthLevelClicked ? 0.3: 0).opacity(forthLevelClicked ? 1.0: 0.0).animation(.easeOut(duration: 4.0), value: forthLevelClicked).onAppear{
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+                        forthLevelClicked.toggle()
+                        firstLevelClicked.toggle()
+                    })
+                }
+            }
+            ZStack {
+                VStack {
+                    if !firstLevelClicked {
+                        Text("Play Me").font(.largeTitle).foregroundColor(.red).offset(y: bounce ? 0 : -5).animation(.interpolatingSpring(stiffness: 100, damping: 5, initialVelocity: 10).repeatForever())
+                    }
+                    playButton(third: thirdLevelClicked).scaleEffect(firstLevelClicked ? 20.0 :0.7 ).rotationEffect( firstLevelClicked ? Angle(degrees: 360): Angle(degrees: 0)).opacity(firstLevelClicked ? 0.5: 1.0).animation(.easeOut(duration: 2.0), value: firstLevelClicked).onTapGesture {
+                        if !firstLevelClicked {
+                            firstLevelClicked.toggle()
+                        }
+                        
+                    }
+                    
+                }.onAppear{
+                    bounce.toggle()
+                }
+                VStack {
+                    Text("SwiftUI")
+                        .font(.system(size: 60))
+                        .background(Color.clear).animation(Animation.easeInOut(duration: 2.0), value: firstLevelClicked)
+                        .offset(x: self.firstLevelClicked ? 0 : 400)
+                    Text("is amazing")
+                        .font(.system(size: 60))
+                        .background(Color.clear)
+                        .animation(Animation.easeInOut(duration: 2.0), value: firstLevelClicked)
+                        .offset(x: self.firstLevelClicked ? 0 : -400)
+                    playButton(third: false).frame(width: 200, height: 200)
+                        .opacity(firstLevelClicked ? 1.0: 0)
+                        .animation(Animation.easeInOut(duration: 2.0), value: firstLevelClicked)
+                        .onTapGesture {
+                            SecondLevelClicked.toggle()
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+                                thirdLevelClicked.toggle()
+                            })
+                        }.rotation3DEffect(.degrees(SecondLevelClicked ? 360: 1), axis: (x: 0, y: SecondLevelClicked ? 1: 0, z: 0)).animation(.easeOut(duration: 2).speed(1.5).repeatForever(autoreverses: false), value: SecondLevelClicked)
+                }
+                
+            }.frame(maxWidth: .infinity, maxHeight: .infinity).scaleEffect(thirdLevelClicked ? 0.008: 1.0).offset(x: thirdLevelClicked ? -80: 0, y: thirdLevelClicked ? 75: 0).animation(.easeOut(duration: 4.0), value: thirdLevelClicked)
+                
+        }
     }
 }
 
---
+++ ShapesAndAnimations/PolygonShape.swift
@@ -0,0 +1,41 @@
+//
+//  PolygonShape.swift
+//  ShapesAndAnimations
+//
+//  Created by rouzbeh on 03.08.23.
+//
+
+import Foundation
+import SwiftUI
+
+struct PolygonShape: Shape {
+    var sides: Int
+    
+    func path(in rect: CGRect) -> Path {
+        // hypotenuse
+        let h = Double(min(rect.size.width, rect.size.height)) / 2.0
+        
+        // center
+        let c = CGPoint(x: rect.size.width / 2.0, y: rect.size.height / 2.0)
+        
+        var path = Path()
+                
+        for i in 0..<sides {
+            let angle = (Double(i) * (360.0 / Double(sides))) * Double.pi / 180
+
+            // Calculate vertex position
+            let pt = CGPoint(x: c.x + CGFloat(cos(angle) * h), y: c.y + CGFloat(sin(angle) * h))
+            
+            if i == 0 {
+                path.move(to: pt) // move to first vertex
+            } else {
+                path.addLine(to: pt) // draw line to next vertex
+            }
+        }
+        
+        path.closeSubpath()
+        
+        return path
+    }
+    
+}

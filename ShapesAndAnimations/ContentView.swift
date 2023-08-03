7346308ad177f6577222c967f363c14ba6b94951
rouzbeh <rouzbehmsv1988@gmail.com>
2023-08-03 10:32:14 +0200

    Initial Commit

---
+++ ShapesAndAnimations/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ShapesAndAnimations
+//
+//  Created by rouzbeh on 03.08.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShapesAndAnimations/ShapesAndAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesAndAnimationsApp.swift
+//  ShapesAndAnimations
+//
+//  Created by rouzbeh on 03.08.23.
+//
+
+import SwiftUI
+
+@main
+struct ShapesAndAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
